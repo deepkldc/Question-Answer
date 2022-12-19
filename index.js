67a7f9c51c22a3118c6d187fe6fc70f6869175fc
Deepanshu Chaudhary <deepkldc@gmail.com>
2022-12-19 19:20:14 +0530

    second commit

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const Question=document.querySelectorAll(".question");
 //console.log(Question)
-
+const Answer=document.querySelectorAll(".first");
+//console.log(Answer);
 Question.forEach(function(ques){
 
 const Btn=ques.querySelector(".ques-btn");
@@ -8,9 +9,15 @@ const Btn=ques.querySelector(".ques-btn");
 
 Btn.addEventListener("click",function(e){
     const Ans=e.currentTarget.parentElement.parentElement.parentElement;
-    console.log(Ans);
-ques.classList.toggle("show-text");
+   // console.log(Ans);
+   Answer.forEach(function(item){
+    if(item !== Ans )
+    {
+      item.classList.remove("show-text");
+    }
+   })
   Ans.classList.toggle("show-text");
+  ques.classList.toggle("show-text");
 })
 })
 
